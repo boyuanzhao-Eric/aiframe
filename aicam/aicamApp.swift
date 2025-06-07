549f1f8731b8bad1bcb81c62ed259b47b5e2f0d9
boyuanzhao-Eric <boyuanzhao534@gmail.com>
2025-06-07 10:29:28 -0400

    Initial Commit

---
+++ aicam/aicamApp.swift
@@ -0,0 +1,17 @@
+//
+//  aicamApp.swift
+//  aicam
+//
+//  Created by Boyuan Zhao on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct aicamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aicamTests/aicamTests.swift
@@ -0,0 +1,16 @@
+//
+//  aicamTests.swift
+//  aicamTests
+//
+//  Created by Boyuan Zhao on 6/7/25.
+//
+
+import Testing
+
+struct aicamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
